e11d31c9a5db6a2090448dd8812781349229595d
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-04-24 10:30:04 -0700

    commit

---
+++ server/controllers/pet.controller.js
@@ -0,0 +1,39 @@
+const Pet = require("../models.pet.model.js");
+
+
+module.exports = {
+
+  createPet: (req, res) => {
+    Pet.create(req.body)
+    .then((newPet) => res.json(newPet))
+    .catch((err) => console.log(err));
+  },
+
+  getOnePet: (req, res) => {
+    Pet.findONe({ _id: req.params.id })
+    .then((onePet) => res.json(OnePet))
+    .catch((err) => console.log(err));
+  },
+
+  getAllPets: (req, res) => {
+    Pet.find({})
+      .then((allPets) => res.json(allPets))
+      .catch((err) => console.log(err));
+  },
+
+  updatePet: (req, res) => {
+    Pet.findbyIdAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    .then((updatePet) => res.json(updatePet))
+    .catch((err) => console.log(err));
+  },
+
+  deletePet: (req, res) => {
+    Pet.deleteOne({ _id: req, res})
+      .then((deleteId) => res.json(deleteId))
+      .catch((err) => console(err));
+  },
+  
+};
---
+++ server/models/pet.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const PetSchema = new mongoose.Schema({
+  name: {
+    type: String, unique: true,
+    required: [ true, "Animal name is required." ],
+    minLength: [ 3, "Name must be a minimum of 3 characters." ]
+},
+  type: {
+    type: String,
+    required: [ true, "Type of animal is required." ],
+    minLength: [ 3, "Type must be a minimum of 3 characters." ]
+},
+  desc: {
+    type: String,
+    required: [ true, "Description is required." ],
+    minLength: [ 3, "Description must be a minimum of 3 characters." ]
+},
+  skill1: {
+    type: String,
+},
+  skill2: {
+    type: String,
+},
+  skill3: {
+    type: String,
+}
+
+}, { timestamps: true });
+
+const Pet = mongoose.model("Pet", PetSchema);
+
+module.exports = Pet;
